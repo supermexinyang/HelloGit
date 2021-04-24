c888b173322f49457fd5ae724a92ea290cb63cd5
SupermeWangXinyang <504935586@qq.com>
2021-04-24 18:29:05 +0800

    更新2

--- HelloGit/src/main/java/com/wangyue/git/HelliGit.java
+++ HelloGit/src/main/java/com/wangyue/git/HelliGit.java
@@ -4,5 +4,6 @@ public class HelliGit {
     public static void main(String[] args) {
         System.out.println("Helli,Git!");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
